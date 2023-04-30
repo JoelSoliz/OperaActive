607e729f244e952466ad43dc20aa04acee59402b
Joel Soliz <joelsolizchoque@gmail.com>
2023-04-29 21:24:22 -0400

    Add more information

--- src/pageSections/home/Events.jsx
+++ src/pageSections/home/Events.jsx
@@ -1,6 +1,7 @@
 import EventCard from "@/components/EventCard";
 import { useEffect } from "react";
 import useEvents from "@/hooks/useEvents";
+import Link from "next/link";
 
 const Events = () => {
   const { events, loading, loadEvents } = useEvents((state) => state);
@@ -17,6 +18,15 @@ const Events = () => {
         {events.map((event, i) => (
           <EventCard value={event} key={i} />
         ))}
+        <Link
+          href="/"
+          className="flex flex-col items-center justify-center font-serif border-solid border-[1px] border-black bg-slate-200 m-2 w-[220px] h-[280px] rounded-xl hover:scale-105 hover:cursor-pointer"
+        >
+          <img src="/assets/logistic.png" />
+          <span className="text-xl font-bold text-slate-600">
+            Ver más eventos...
+          </span>
+        </Link>
       </div>
     </div>
   );
